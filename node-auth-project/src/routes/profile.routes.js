24b9ed20019965a82906c342a172eae2db333514
vizarz <147136713+vizarz@users.noreply.github.com>
2025-03-22 21:21:22 +0300

    Delete node-auth-project directory

--- node-auth-project/src/routes/profile.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { getProfile } = require('../controllers/profile.controller')
-
-router.get('/', getProfile)
-
-module.exports = router
